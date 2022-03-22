fcd3c4d6bd0c0a5d96ec348e1a1ebbf71aa56d79
Rd Tomaub <70382383+shindelzare@users.noreply.github.com>
2022-03-22 18:45:26 +0800

    Update Operations.cs

--- Operations.cs
+++ Operations.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,11 +21,10 @@ namespace CFPL
         public List<string> ErrorMessages { get { return errorMessages; } }
         public List<Tokens> infixToPostFix()
         {
-            int ctr = 0;
+            int ctr = 0, len = infixTokens.Count;
             List<Tokens> postfix = new List<Tokens>();
             Stack<Tokens> stack = new Stack<Tokens>();
-            int len = infixTokens.Count;
-            string str = "";
+            
             while (ctr < len)
             {
                 if (infixTokens[ctr].Lexeme[0] == '(')
@@ -38,11 +37,9 @@ namespace CFPL
                 else if (isOperator(infixTokens[ctr].Lexeme[0]))
                 {
                     // check precedence of the scanned operator and the stacked operator
-                    while (stack.Count != 0 && isOperator(infixTokens[ctr].Lexeme[0]) && (getPrecedence(infixTokens[ctr].Lexeme[0]) <= getPrecedence(stack.Peek().Lexeme[0])))
-                    {
+                    while (stack.Count != 0 && isOperator(infixTokens[ctr].Lexeme[0]) && (getPrecedence(infixTokens[ctr].Lexeme) <= getPrecedence(stack.Peek().Lexeme)))
                         postfix.Add(stack.Pop());
-                    }
-
+                    
                     stack.Push(infixTokens[ctr]);
                 }
                 else if (infixTokens[ctr].Lexeme[0] == ')')
@@ -59,7 +56,6 @@ namespace CFPL
                     errorMessages.Add(string.Format("Invalid equation at line" + infixTokens[ctr].Line + 1));
                     break;
                 }
-                Console.WriteLine(str);
                 ctr++;
             }
             while (stack.Count != 0)
@@ -68,14 +64,162 @@ namespace CFPL
             }
             return postfix;
         }
-        /*public List<Tokens> boolInfixToPostfix()
+        public List<Tokens> logicInfixToPostFix()
         {
+            int ctr = 0, len = infixTokens.Count;
             List<Tokens> postfix = new List<Tokens>();
             Stack<Tokens> stack = new Stack<Tokens>();
-        }*/
+
+            while (ctr < len)
+            {
+                if (infixTokens[ctr].Lexeme[0] == '(')
+                    stack.Push(infixTokens[ctr]);
+                else if (infixTokens[ctr].Type == TokenType.IDENTIFIER || infixTokens[ctr].Type == TokenType.FLOAT_LIT
+                || infixTokens[ctr].Type == TokenType.INT_LIT || infixTokens[ctr].Type == TokenType.BOOL_LIT)
+                {
+                    postfix.Add(infixTokens[ctr]);
+                }
+                else if (isLogicOperator(infixTokens[ctr].Lexeme))
+                {
+                    // check precedence of the scanned operator and the stacked operator
+                    while (stack.Count != 0 && isLogicOperator(infixTokens[ctr].Lexeme) && (getPrecedence(infixTokens[ctr].Lexeme) <= getPrecedence(stack.Peek().Lexeme)))
+                    {
+                        postfix.Add(stack.Pop());
+                    }
+                    stack.Push(infixTokens[ctr]);
+                }
+                else if (infixTokens[ctr].Lexeme[0] == ')')
+                {
+                    while (stack.Peek().Type != TokenType.LEFT_PAREN)
+                    {
+                        postfix.Add(stack.Pop());
+                    }
+                    if (stack.Peek().Type == TokenType.LEFT_PAREN)
+                        stack.Pop();
+                }
+                else
+                {
+                    errorMessages.Add(string.Format("Invalid equation at line" + infixTokens[ctr].Line + 1));
+                    break;
+                }
+                ctr++;
+            }
+            while (stack.Count != 0)
+                postfix.Add(stack.Pop());
+            
+            return postfix;
+        }
+        public bool evaluateBooleanExpression(List<Tokens> postfix)
+        {
+            bool flag = false;
+            Stack<string> stack = new Stack<string>();
+            List<string> postfixValues = new List<string>();
+            int ctr = 0, res=0, len = postfix.Count;
+            string val;
+            Console.WriteLine("VAR COUNT " + variables.Count);
+            foreach (KeyValuePair<string, object> x in variables)
+                Console.WriteLine(x.Key + ", " + x.Value);
+            //copying
+            while (ctr < len)
+            {
+                if (postfix[ctr].Type == TokenType.IDENTIFIER)
+                {
+                    val = Convert.ToString(variables[postfix[ctr].Lexeme]);
+                    postfixValues.Add(val);
+                }
+                else if (postfix[ctr].Type == TokenType.FLOAT_LIT || postfix[ctr].Type == TokenType.INT_LIT 
+                    || postfix[ctr].Type == TokenType.BOOL_LIT)
+                {
+                    val = postfix[ctr].Lexeme;
+                    postfixValues.Add(val.ToString());
+                }
+                else if (isLogicOperator(postfix[ctr].Lexeme))
+                    postfixValues.Add(postfix[ctr].Lexeme);
+                else
+                {
+                    errorMessages.Add(string.Format("Invalid infix expression at line" + infixTokens[ctr].Line + 1));
+                    break;
+                }
+                ctr++;
+            }
+            ctr = 0;
+            // postfix Values now contains a string of float_lit, int_lit, bool_lit, and operators
+            while (ctr < len)
+            {
+                if (postfix[ctr].Type == TokenType.INT_LIT || postfix[ctr].Type == TokenType.FLOAT_LIT
+                || postfix[ctr].Type == TokenType.IDENTIFIER || postfix[ctr].Type == TokenType.BOOL_LIT)
+                {
+                    Console.WriteLine("Pushed " + postfix[ctr].Lexeme + ", " + postfix[ctr].Literal + " to the stack");
+                    stack.Push(postfixValues[ctr]);
+                }
+                else if (isLogicOperator(postfixValues[ctr]))
+                {
+                    string i1 = stack.Pop();
+                    string i2 = stack.Pop();
+                    if (isOperator(postfixValues[ctr][0]))
+                    {
+                        int n1 = int.Parse(i1);
+                        int n2 = int.Parse(i2);
+                        if (postfixValues[ctr] == "+")
+                            res = n2 + n1;
+                        else if (postfixValues[ctr] == "-")
+                            res = n2 - n1;
+                        else if (postfixValues[ctr] == "*")
+                            res = n2 * n1;
+                        else if (postfixValues[ctr] == "/")
+                            res = n2 / n1;
+                        else if (postfixValues[ctr] == "%")
+                            res = n2 % n1;
+                        stack.Push(res.ToString());
+                    }
+                    else if (isRelational(postfixValues[ctr]))
+                    {
+                        int n1 = int.Parse(i1);
+                        int n2 = int.Parse(i2);
+                        if (postfixValues[ctr] == ">")
+                            flag = n2 > n1;
+                        else if (postfixValues[ctr] == ">=")
+                            flag = n2 >= n1;
+                        else if (postfixValues[ctr] == "==")
+                            flag = n2 == n1;
+                        else if (postfixValues[ctr] == "<>")
+                            flag = n2 != n1;
+                        else if (postfixValues[ctr] == "<")
+                            flag = n2 < n1;
+                        else if (postfixValues[ctr] == "<=")
+                            flag = n2 <= n1;
+
+                        stack.Push(flag.ToString());
+                    }
+                    else if (isLogic(postfixValues[ctr]))
+                    {
+                        bool b1 = Convert.ToBoolean(i1);
+                        bool b2 = Convert.ToBoolean(i2);
+                        if (postfixValues[ctr] == "AND")
+                            flag = b2 && b1;
+                        else if (postfixValues[ctr] == "OR")
+                            flag = b2 || b1;
+
+                        stack.Push(flag.ToString());
+                    }
+                    else
+                        Console.Write("ELSE ASDASD");
+                    
+                }
+                ctr++;
+            }
+            Console.WriteLine("STACK COUNT " + stack.Count());
+            if(stack.Count != 0)
+            {
+                Console.WriteLine("STACK POPPED");
+                Console.WriteLine("STRING "+ stack.Peek().ToString().ToLower());
+                flag = bool.Parse(stack.Pop().ToString().ToLower());
+            }
+            return flag;
+        }
         public int evaluateIntegerExpression(List<Tokens> postfix)
         {
-            int res = -99;
+            int res = 0;
             Stack<int> stack = new Stack<int>();
             List<string> postfixValues = new List<string>();
             int ctr = 0, val;
@@ -90,7 +234,7 @@ namespace CFPL
                 }
                 else if (postfix[ctr].Type == TokenType.FLOAT_LIT || postfix[ctr].Type == TokenType.INT_LIT)
                 {
-                    val = (int)postfix[ctr].Literal;
+                    val = int.Parse(postfix[ctr].Lexeme);
                     postfixValues.Add(val.ToString());
                 }
                 else if (isOperator(postfix[ctr].Lexeme[0]))
@@ -173,12 +317,12 @@ namespace CFPL
                 if (postfix[ctr].Type == TokenType.INT_LIT || postfix[ctr].Type == TokenType.FLOAT_LIT
                 || postfix[ctr].Type == TokenType.IDENTIFIER)
                 {
-                    stack.Push(double.Parse(postfixValues[ctr]));
+                    stack.Push(Convert.ToDouble(postfixValues[ctr]));
                 }
                 else if (isOperator(postfixValues[ctr][0]))
                 {
-                    double n1 = (double)stack.Pop();
-                    double n2 = (double)stack.Pop();
+                    double n1 = Convert.ToDouble(stack.Pop());
+                    double n2 = Convert.ToDouble(stack.Pop());
                     if (postfixValues[ctr] == "+")
                         res = n2 + n1;
                     if (postfixValues[ctr] == "-")
@@ -200,6 +344,7 @@ namespace CFPL
         /*
          public bool evaluateBooleanExpression(List<Tokens> postfix)
         {
+
         }
         */
 
@@ -207,14 +352,32 @@ namespace CFPL
         {
             return (x == '+' || x == '-' || x == '*' || x == '/' || x == '%');
         }
-        public int getPrecedence(char symbol) // for binary only
+        public bool isLogicOperator(string x) 
         {
-            if (symbol == '*' || symbol == '/' || symbol == '%') // highest precedence
+            return (x == "+" || x == "-" || x == "*" || x == "/" || x == "%" || x == "AND" || x == "OR" || x == "<"
+                 || x == "<=" || x == "==" || x == "<>" || x == ">" || x == ">=");
+        }
+        public bool isRelational(string x)
+        {
+            return x == "<" || x == "<=" || x == "==" || x == "<>" || x == ">" || x == ">=";
+        }
+        public bool isLogic(string x)
+        {
+            return x == "AND" || x == "OR";
+        }
+        public int getPrecedence(string symbol) 
+        {
+            if (symbol == "*" || symbol == "/" || symbol == "%") // highest precedence
+                return 4;
+            else if (symbol == "+" || symbol == "-")
+                return 3;
+            else if (symbol == ">" || symbol == ">=" || symbol == "==" || symbol == "<>" || symbol == "<" || symbol == "<=") // relational operators
                 return 2;
-            else if (symbol == '+' || symbol == '-')          // lowest precedence 
+            else if (symbol == "AND" || symbol == "OR") // lowest precedence, logic operators
                 return 1;
             else
                 return 0;
         }
     }
 }
+
